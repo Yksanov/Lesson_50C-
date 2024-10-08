3d00c5e0da6dc8532d628f3995244adebb7573dd
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-08 16:08:15 +0600

    change code to ZooStoreContext

--- WebApplication2/Models/ZooStoreContext.cs
+++ WebApplication2/Models/ZooStoreContext.cs
@@ -4,6 +4,6 @@ namespace WebApplication2.Models;
 
 public class ZooStoreContext : DbContext
 {
-    public DbSet<Animal> Phones { get; set; }
+    public DbSet<Animal> Animals { get; set; }
     public ZooStoreContext(DbContextOptions<ZooStoreContext> options) : base(options) { }
 }
