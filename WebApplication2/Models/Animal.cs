f013dc54c2f36d1aa94eea82d5805e6f916b6d4b
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-08 12:53:23 +0600

    Added model Animal

---
+++ WebApplication2/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace WebApplication2.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Type { get; set; }
+    public string Color { get; set; }
+    public double Weight { get; set; }
+    public int Price { get; set; }
+}
