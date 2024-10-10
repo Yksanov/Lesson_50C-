bb54113d7c10eaa419ebbbeae13f570b62aa4814
Eldos Yksanov <eldosyksanov@gmail.com>
2024-10-10 18:44:19 +0600

    Doing 3-Task and change code to Details

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279c82684014c0db5805b5e4d8836d121781e67d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60bab524328b61c50c3a274a890e5626c657a45d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
